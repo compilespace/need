305ee3d13a26c9e3659866ab9634c552603fb917
compilespace <879598552@qq.com>
2019-03-15 14:48:24 +0800

    奉献给广大人民

---
+++ src/main/java/com/compilespace/need/utils/AnalysisObjectUtil.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C), 2018-2019, XXX有限公司
+ * FileName: AnalysisObject
+ * Author:   Admin
+ * Date:     2019/3/15 14:07
+ * Description: 解析复杂对象工具类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.compilespace.need.utils;
+
+import org.springframework.beans.BeanUtils;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈解析复杂对象工具类,比如List<Object>〉
+ *
+ * @author Admin
+ * @create 2019/3/15
+ * @since 1.0.0
+ */
+public class AnalysisObjectUtil {
+    /**
+     * @param source 需要处理的复杂对象
+     * @param strArr 复杂对象中的get属性,也就是它对应的变量
+     * @return Map<String   ,       Object> 将复杂对象从新封装成Map返回
+     */
+    public static Map<String, Object> manageObjectProperty(Object source, String[] strArr) {
+        Map<String, Object> map = new HashMap<>();
+        if (source == null)
+            return null;
+        if (strArr == null || strArr.length < 1) {
+            return null;
+        }
+        for (String str : strArr) {
+            PropertyDescriptor sourcePd = BeanUtils.getPropertyDescriptor(
+                    source.getClass(), str);
+            if (sourcePd != null && sourcePd.getReadMethod() != null) {
+                try {
+                    Method readMethod = sourcePd.getReadMethod();
+                    if (!Modifier.isPublic(readMethod.getDeclaringClass()
+                            .getModifiers())) {
+                        readMethod.setAccessible(true);
+                    }
+                    Object value = readMethod.invoke(source, new Object[0]);
+                    //如果是时间,就格式化一下:
+                    if (value instanceof Date) {
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        value = sdf.format(value);
+                    }
+                    //map装值
+                    map.put(str, value);
+                } catch (Exception ex) {
+                    throw new RuntimeException(
+                            "Could not copy properties from source to target",
+                            ex);
+                }
+            }
+        }
+        return map;
+    }
+}
---
+++ src/main/java/com/compilespace/need/utils/ExcelUtil.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C), 2018-2019, XXX有限公司
+ * FileName: ExcelUtil
+ * Author:   Admin
+ * Date:     2019/3/15 13:16
+ * Description: Excel表格上传下载工具类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.compilespace.need.utils;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈Excel表格上传下载工具类〉
+ *
+ * @author Admin
+ * @create 2019/3/15
+ * @since 1.0.0
+ */
+public class ExcelUtil {
+
+    /**
+     * 从数据库中导出数据到Excel表格(下载)
+     * @param heads Excel表头信息
+     * @param datas 将各个实体类取值封装到String数组里
+     * @return SXSSFWorkbook
+     */
+    public SXSSFWorkbook downloadExcel(String[] heads, List<String[]> datas) {
+        // 创建excel对象
+        SXSSFWorkbook workbook = new SXSSFWorkbook();
+        // 创建excel表
+        Sheet sheet = workbook.createSheet();
+        // 创建表头行
+        Row headRow = sheet.createRow(0);
+        // 给表头设置数据
+        for (int i = 0; i < heads.length; i++) {
+            // 创建行中的列
+            Cell createCell = headRow.createCell(i);
+            // 在列中设置值
+            createCell.setCellValue(heads[i]);
+        }
+        // 给表体设置数据
+        for (int j = 0; j < datas.size(); j++) {
+            // 创建行
+            Row bodyRow = sheet.createRow(j + 1);
+            // 将集合转成数组
+            String[] str = datas.get(j);
+            // 给列中设置数据
+            for (int i = 0; i < str.length; i++) {
+                // 创建列
+                Cell createCell = bodyRow.createCell(i);
+                // 在列中设置值
+                createCell.setCellValue(str[i]);
+            }
+        }
+        return workbook;
+    }
+
+    /**
+     * 从Excel表格导入数据库(上传)
+     * 调用此方法需注意以下几项:
+     * 1.Excel表格中不能有空的空格
+     * 2.数字和字符请严格区分
+     *
+     * @param uploadFile 要上传的Excel文件
+     * @return List<String                                                               [                                                               ]> 封装好的实体类数据,只要遍历将数组设置进实体类中即可
+     * @throws Exception
+     */
+    public List<String[]> uploadExcel(MultipartFile uploadFile) throws Exception {
+        //准备容器
+        List<String[]> datas = new ArrayList<>();
+        InputStream inputStream = uploadFile.getInputStream();
+        //创建一个SXSSFWorkbook
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+        //拿到第一张表
+        XSSFSheet sht = workbook.getSheetAt(0);
+        //拿到总行数
+        int lastRowNum = sht.getLastRowNum();
+        for (int i = 1; i <= lastRowNum; i++) {
+            //拿到某一行
+            Row row = sht.getRow(i);
+            //拿到这一行有多少列
+            short lastCellNum = row.getLastCellNum();
+            //装每一行的容器
+            String[] rowData = new String[lastCellNum];
+            for (int j = 0; j < lastCellNum; j++) {
+                //把数据放进去
+                rowData[j] = row.getCell(j).getStringCellValue();
+            }
+            //把这一行放到容器中
+            datas.add(rowData);
+        }
+        inputStream.close();
+        return datas;
+    }
+}
+
